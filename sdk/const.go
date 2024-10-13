f30bbd151975ce62ccc11a8cd1a994bb605be1dd
Andrew Donelson <nlaakald@gmail.com>
2024-10-12 20:05:40 -0500

    bump

--- sdk/const.go
+++ sdk/const.go
@@ -4,59 +4,59 @@ package sdk
 
 const (
 	// Blockchain Identification
-	BlockchainName           = "Go Basic Blockchain"
-	BlockchainSymbol         = "GBB"
-	BlockchainVersion        = "0.2.0"
-	BlockchainOrganizationID = 1 // 1 is reserved for this blockchain "Go Basic Blockchain"
-	BlockchainAdminUserID    = 1
-	BlockchainAppID          = 1 // 1 is reserved for this blockchain's Core
-	BlockchainDevAssetID     = 1
-	BlockchainMinerAssetID   = 2
+	BlockchainName          = "Go Basic Blockchain"
+	BlockchainSymbol        = "GBB"
+	BlockchainVersion       = "0.1.0"
+	BlockhainOrganizationID = 1 // 1 is reserved for this blockchain "Go Basic Blockchain"
+	BlockchainAdminUserID   = 1
+	BlockchainAppID         = 1 // 1 is reserved for this blockchain's Core
+	BlockchainDevAssetID    = 1
+	BlockchainMinerAssetID  = 2
 
 	// Blockchain Parameters
-	BlockTimeInSec        = 5
-	ProofOfWorkDifficulty = 4
-	TransactionFee        = 0.05    // 5 hundredths of a coin (a nickel-ish)
-	MinTransactionFee     = 0.01    // Minimum transaction fee
-	MinerRewardPCT        = 50.0    // Miner reward is 50% of the transaction fee
-	DevRewardPCT          = 50.0    // Developer reward is 50% of the transaction fee
+	blockTimeInSec        = 5
+	proofOfWorkDifficulty = 4
+	transactionFee        = 0.05    // 5 hundredths of a coin (a nickel-ish)
+	minTransactionFee     = 0.01    // Minimum transaction fee
+	minerRewardPCT        = 50.0    // Miner reward is 50% of the transaction fee
+	devRewardPCT          = 50.0    // Developer reward is 50% of the transaction fee
 	MaxBlockSize          = 1000000 // Maximum block size in bytes (1MB)
-	IndexCacheSize        = 65536   // Size of the block/transaction index cache (1,572,864 bytes or 1.5 MB)
+	indexCacheSize        = 65536   // Size of the block/transaction index cache (1,572,864 bytes or 1.5 MB)
 
 	// Token Related
-	TokenCount       = 33554432
-	TokenPrice       = 0.01 // Price of a token in USD
-	AllowNewTokens   = false
-	FundWalletAmount = 100.0 // Default amount to fund new wallets
+	tokenCount       = 33554432
+	tokenPrice       = 0.01 // Price of a token in USD
+	allowNewTokens   = false
+	fundWalletAmount = 100.0 // Default amount to fund new wallets
 
 	// Network Settings
-	APIHostname = ":8100"
-	P2PHostname = ":8101"
+	apiHostname = ":8100"
+	p2pHostname = ":8101"
 
 	// Default Addresses
-	MinerAddress = "MINER" // Will be supplied by the environment
-	DevAddress   = "DEV"   // Will be supplied by the genesis block
+	minerAddress = "MINER" // Will be supplied by the environment
+	devAddress   = "DEV"   // Will be supplied by the genesis block
 
 	// Data Storage
-	DataFolder   = "../data"
-	WalletFolder = DataFolder + "/wallets"
-	BlockFolder  = DataFolder + "/blocks"
-	ConfigFile   = "../../.local.env"
+	dataFolder   = "../data"
+	walletFolder = dataFolder + "/wallets"
+	blockFolder  = dataFolder + "/blocks"
+	cfgFile      = "../../.local.env"
 
 	// Email Settings
-	GmailEmail    = ""
-	GmailPassword = "" // Should be supplied by the environment
+	gmailEmail    = ""
+	gmailPassword = "" // Should be supplied by the environment
 
 	// Feature Flags
 	EnableAPI = true
-	Verbose   = true
+	verbose   = true
 
 	// Cryptographic Constants
-	SaltSize = 32
-	MaxNonce = 12 // bytes
+	saltSize = 32
+	maxNonce = 12 // bytes
 
 	// Formatting
-	LogDateTimeFormat = "2006-01-02 15:04:05"
+	logDateTimeFormat = "2006-01-02 15:04:05"
 
 	// Protocol Versions
 	TransactionProtocolVersion = "1.0"
